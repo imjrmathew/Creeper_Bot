a449c63a9a096efbe5fb767bb9ab582b3afb5377
Don Mathew <iamdonmathew@gmail.com>
2021-01-23 15:23:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const { reset } = require('nodemon');
 const client = new Discord.Client();
 require('dotenv').config();
 
-const oldchannelid = '761303460460625984';
-const newchannelid = '796454712353226783';
+const oldchannelid = 'OLD__CHANNELID';
+const newchannelid = 'NEW__CHANNELID';
 const port = process.env.PORT || 4000;
 
 client.on('ready', () => {
@@ -48,51 +48,29 @@ client.on('message', msg => {
 
 
 async function mute(msg) {
-    if(msg.member.id === '689685969250943156') {
+    if(msg.member.id === 'MEMBER__ID') {
         const channel = msg.guild.channels.cache.get(oldchannelid);
         await channel.members.map(m => m.voice.setMute(true));
         msg.reply('All members are muted.');
     } else {
-        msg.reply("Fuck Off, You don't have permission to do this things! 🤣")
-    }
-}
-
-
-async function muteOnly(msg, target) {
-    if(msg.member.id === '689685969250943156') {
-        const channel = msg.guild.channels.cache.get(oldchannelid);
-        await channel.members.map( m => m.voice.setMute(true));
-        msg.channel.send(`${target.username} has been muted.`)
-    } else {
-        msg.reply("Fuck Off, You don't have permission to do this things! 🤣")
+        msg.reply("You don't have permission to do this things! 🤣")
     }
 }
 
 
 async function unmute(msg) {
-    if(msg.member.id === '689685969250943156') {
+    if(msg.member.id === 'MEMBER__ID') {
         const channel = msg.guild.channels.cache.get(oldchannelid);
         await channel.members.map(m => m.voice.setMute(false));
         msg.reply('All members are unmuted.');
     } else {
-        msg.reply("Fuck Off, You don't have permission to do this things! 🤣")
-    }
-}
-
-
-async function unmuteOnly(msg, target) {
-    if(msg.member.id === '689685969250943156') {
-        const channel = msg.guild.channels.cache.get(oldchannelid);
-        await channel.members.map( m => m.voice.setMute(false));
-        msg.channel.send(`${target.username} has been unmuted.`)
-    } else {
-        msg.reply("Fuck Off, You don't have permission to do this things! 🤣")
+        msg.reply("You don't have permission to do this things! 🤣")
     }
 }
 
 
 async function move(msg, taggedUser) {
-    if(msg.member.id === '689685969250943156') {
+    if(msg.member.id === 'MEMBER__ID') {
         taggedUser.map( async e => {
             const member = msg.guild.members.cache.get(e.id);
             await member.voice.setChannel(newchannelid);
@@ -100,17 +78,17 @@ async function move(msg, taggedUser) {
         });
         await msg.reply('Dead Bodies are moved.');
     } else {
-        msg.reply("Fuck Off, You don't have permission to do this things! 🤣")
+        msg.reply("You don't have permission to do this things! 🤣")
     }
 }
 
 async function resetUsers(msg) {
-    if(msg.member.id === '689685969250943156') {
+    if(msg.member.id === 'MEMBER__ID') {
         const channel = msg.guild.channels.cache.get(newchannelid);
         await channel.members.map(m => m.voice.setChannel(oldchannelid));
         msg.reply('Revived.');
     } else {
-        msg.reply("Fuck Off, You don't have permission to do this things! 🤣")
+        msg.reply("You don't have permission to do this things! 🤣")
     }
 }
 
